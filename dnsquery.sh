4c52551836fad2f8dd6b812d29bf278239e31b52
Fatih Çiroğlu <kaptankojiro@gmail.com>
2021-11-20 21:57:14 +0300

    Update dnsquery.sh

--- dnsquery.sh
+++ dnsquery.sh
@@ -7,10 +7,18 @@ echo "Downloading Alexa Top 1 Million Sites"
 echo ""
 wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
 unzip top-1m.csv.zip
-rm sonuc1.csv
-echo DomainName,EntropyResult[1],EntropyResult[2],RegistrationDate,UpdatedDate,AlexaWordCount,IP,Region,City > sonuc1.csv
+
+#!/bin/bash
+figlet -c -w 100 Sysmon DNS Threat Hunting
+echo ""
+echo "This scripts requires --> whois,freq.py,Alexa top1-m.csv,figlet,libreoffice,jq"
+echo ""
+
+rm result.csv
+echo DomainName,EntropyResult[1],EntropyResult[2],RegistrationDate,UpdatedDate,AlexaWordCount,IP,Region,City,AlienVault,Talos,ThreatCrowd > result.csv
 cat dnsQueries.csv | sort | uniq | grep -v "_" | grep "\b$word\." | grep -v "," > sysmonDNSlist.txt
 echo "Query process is started."
+
 input="sysmonDNSlist.txt"
 while IFS= read -r line
 do
@@ -24,10 +32,9 @@ output6=($(curl -s http://ip-api.com/json/$line | jq '.region' | tr -d '"' ))
 output7=($(curl -s http://ip-api.com/json/$line| jq '.city' | tr -d '"' ))
 output8=($(echo https://otx.alienvault.com/indicator/domain/$line ))
 output9=($(echo https://talosintelligence.com/reputation_center/lookup?search=$line ))
-output10=($(echo https://otx.alienvault.com/indicator/domain/$line ))
-output11=($(echo https://www.threatcrowd.org/domain.php?domain=$line ))
+output10=($(echo https://www.threatcrowd.org/domain.php?domain=$line ))
 
 
-echo $line,"${output[0]}""${output[1]}","$output2[0]","${output3[0]}","${output4[0]},${output5[0]},${output6[0]},${output7[0]},${output8[0]},${output9[0]},${output10[0]},${output11n[0]}"|  tr -d ')' | tr -d '(' >> sonuc1.csv
+echo $line,"${output[0]}""${output[1]}","$output2[0]","${output3[0]}","${output4[0]},${output5[0]},${output6[0]},${output7[0]},${output8[0]},${output9[0]},${output10[0]}" |  tr -d ')' | tr -d '(' >> result.csv
 done < "$input"
-cat sonuc1.csv
+libreoffice result.csv
